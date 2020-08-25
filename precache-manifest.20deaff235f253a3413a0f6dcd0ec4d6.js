2dbbdec88a950f07771449c484917982031fc7d9
T145 <T145@protonmail.com>
2020-08-25 17:06:04 -0400

    Updates

---
+++ precache-manifest.20deaff235f253a3413a0f6dcd0ec4d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fd3f8e7db174bf02538d05674e04d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33aec6191c9fa25518de",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4cfa40760d8e67cc9af",
+    "url": "/static/js/2.f39ab070.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f39ab070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33aec6191c9fa25518de",
+    "url": "/static/js/main.6793275f.chunk.js"
+  },
+  {
+    "revision": "4dfacaf1320f122f0dd1",
+    "url": "/static/js/runtime-main.fa6ff98c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
