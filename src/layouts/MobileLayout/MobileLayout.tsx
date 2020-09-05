103c9f22bb0a0e94cb74696d3b9083296724d7df
T145 <T145@protonmail.com>
2020-09-05 18:54:25 -0400

    Mobile baseline test

--- src/layouts/MobileLayout/MobileLayout.tsx
+++ src/layouts/MobileLayout/MobileLayout.tsx
@@ -2,38 +2,29 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { isIOS } from 'react-device-detect';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Paper from '@material-ui/core/Paper';
-import Fab from '@material-ui/core/Fab';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import Avatar from '@material-ui/core/Avatar';
+import {
+  CssBaseline,
+  AppBar,
+  SwipeableDrawer,
+  Toolbar,
+  Typography,
+  IconButton,
+  Fab,
+  List,
+} from '@material-ui/core';
+
 import MenuIcon from '@material-ui/icons/Menu';
 import AddIcon from '@material-ui/icons/Add';
 import SearchIcon from '@material-ui/icons/Search';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import Routes from '../../routes';
+import ListLink from '../../components/ListLink/ListLink';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     text: {
       padding: theme.spacing(2, 2, 0),
-    },
-    paper: {
       paddingBottom: 50,
-    },
-    list: {
-      marginBottom: theme.spacing(2),
-    },
-    subheader: {
-      backgroundColor: theme.palette.background.paper,
     },
     appBar: {
       top: 'auto',
@@ -50,11 +41,23 @@ const useStyles = makeStyles((theme: Theme) =>
       right: 0,
       margin: '0 auto',
     },
+    drawer: {
+      width: 'auto',
+    },
   })
 );
 
 export default function MobileLayout() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Router>
@@ -75,7 +78,38 @@ export default function MobileLayout() {
 
       <AppBar position="fixed" color="primary" className={classes.appBar}>
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="open drawer">
+          <SwipeableDrawer
+            anchor="bottom"
+            open={open}
+            onOpen={handleDrawerOpen}
+            onClose={handleDrawerClose}
+            disableBackdropTransition={!isIOS}
+            disableDiscovery={isIOS}
+          >
+            <div
+              className={classes.drawer}
+              role="presentation"
+              onClick={handleDrawerClose}
+              onKeyDown={handleDrawerClose}
+            >
+              <List>
+                {Routes.map(route => (
+                  <ListLink
+                    to={route.path}
+                    primary={route.name}
+                    icon={route.icon}
+                    key={route.key}
+                  />
+                ))}
+              </List>
+            </div>
+          </SwipeableDrawer>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+          >
             <MenuIcon />
           </IconButton>
 
