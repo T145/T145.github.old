3004db83406857f8bc4d6a0847f3523f68bdac41
T145 <T145@protonmail.com>
2020-09-05 21:49:44 -0400

    Now using react icons

--- src/components/DesktopNavigator/DesktopNavigator.tsx
+++ src/components/DesktopNavigator/DesktopNavigator.tsx
@@ -9,10 +9,7 @@ import {
   Tooltip,
   IconButton,
 } from '@material-ui/core';
-
-import GitHubIcon from '@material-ui/icons/GitHub';
-import PaletteIcon from '@material-ui/icons/PaletteRounded';
-
+import { VscGithub as GithubIcon } from 'react-icons/vsc';
 import Routes from '../../routes';
 import DesktopStyle from '../../constants/DesktopStyle/DesktopStyle';
 import ListLink from '../ListLink/ListLink';
@@ -34,17 +31,6 @@ function DesktopNavigator() {
 
           <div className={classes.content} />
 
-          <Tooltip title="Edit colors" enterDelay={300}>
-            <IconButton
-              color="inherit"
-              aria-label="Edit colors"
-              data-ga-event-category="header"
-              data-ga-event-action="colors"
-            >
-              <PaletteIcon />
-            </IconButton>
-          </Tooltip>
-
           <Tooltip title="Github" enterDelay={300}>
             <IconButton
               color="inherit"
@@ -54,7 +40,7 @@ function DesktopNavigator() {
               data-ga-event-category="header"
               data-ga-event-action="github"
             >
-              <GitHubIcon />
+              <GithubIcon />
             </IconButton>
           </Tooltip>
         </Toolbar>
--- src/components/MobileNavigator/MobileNavigator.tsx
+++ src/components/MobileNavigator/MobileNavigator.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import { isIOS } from 'react-device-detect';
-
 import {
   AppBar,
   Toolbar,
@@ -11,12 +10,11 @@ import {
   SwipeableDrawer,
   List,
 } from '@material-ui/core';
-
-import MenuIcon from '@material-ui/icons/Menu';
-import AddIcon from '@material-ui/icons/Add';
-import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert';
-
+import { FaMapSigns as SignIcon } from 'react-icons/fa';
+import {
+  FiChevronsUp as ChevronIcon,
+  FiMoreVertical as MoreIcon,
+} from 'react-icons/fi';
 import Routes from '../../routes';
 import MobileStyle from '../../constants/MobileStyle/MobileStyle';
 import ListLink from '../ListLink/ListLink';
@@ -74,7 +72,7 @@ function MobileNavigator() {
           aria-label="open drawer"
           onClick={handleDrawerOpen}
         >
-          <MenuIcon />
+          <SignIcon />
         </IconButton>
 
         <Route>
@@ -86,14 +84,10 @@ function MobileNavigator() {
         </Route>
 
         <Fab color="secondary" aria-label="add" className={classes.fabButton}>
-          <AddIcon />
+          <ChevronIcon />
         </Fab>
 
         <div className={classes.grow} />
-
-        <IconButton color="inherit">
-          <SearchIcon />
-        </IconButton>
 
         <IconButton edge="end" color="inherit">
           <MoreIcon />
