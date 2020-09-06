070d1c6762d0b43822f402241c0b3f0fd5680b26
T145 <T145@protonmail.com>
2020-09-06 17:48:02 -0400

    Mobile footer fix

--- src/layouts/MobileLayout/MobileLayout.tsx
+++ src/layouts/MobileLayout/MobileLayout.tsx
@@ -60,18 +60,22 @@ export default function MobileLayout() {
 
   return (
     <Router>
-      <Typography className={classes.text} variant="h5" gutterBottom>
-        <Switch>
-          {Routes.map(route => (
-            <Route
-              exact
-              path={route.path}
-              key={route.key}
-              component={route.component}
-            />
-          ))}
-        </Switch>
-      </Typography>
+      <main>
+        <Typography className={classes.text} variant="h5" gutterBottom>
+          <Switch>
+            {Routes.map(route => (
+              <Route
+                exact
+                path={route.path}
+                key={route.key}
+                component={route.component}
+              />
+            ))}
+          </Switch>
+        </Typography>
+
+        <Footer />
+      </main>
 
       <ElevationScroll>
         <AppBar position="fixed" color="primary" className={classes.appBar}>
@@ -136,8 +140,6 @@ export default function MobileLayout() {
           </Toolbar>
         </AppBar>
       </ElevationScroll>
-
-      <Footer />
     </Router>
   );
 }
