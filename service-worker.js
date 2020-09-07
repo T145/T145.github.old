408be3f5c8612cc97ea4dbd2af22689420997ad1
T145 <T145@protonmail.com>
2020-09-07 10:48:06 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8dfa2249f76a382eaa74700020afb5c2.js"
+  "/precache-manifest.2cd730e204b42940fb02f7ce3d204647.js"
 );
 
 self.addEventListener('message', (event) => {
