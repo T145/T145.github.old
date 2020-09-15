fd08e70c7e59debe74255c11a3dca80ba19b75ce
T145 <T145@protonmail.com>
2020-09-15 02:24:57 -0400

    Slimmed badge margin

--- src/pages/ChestsPage/ChestsPage.tsx
+++ src/pages/ChestsPage/ChestsPage.tsx
@@ -10,7 +10,7 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
     },
     paper: {
-      padding: theme.spacing(1),
+      padding: theme.spacing(0.1),
       textAlign: 'center',
       color: theme.palette.text.secondary,
     },
